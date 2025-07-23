cbf9cde54279c9ed7b3b87a7ff617870e064625a
Amit Achari <amit.achari06@outlook.com>
2025-07-24 01:09:08 +0530

    My first commit

---
+++ Main.tf
@@ -0,0 +1,25 @@
+
+resource "azurerm_resource_group" "myRG" {
+  name     = "Terraform_RG"
+  location = "East US"
+
+}
+resource "random_string" "storage_account_name_unique" {
+  length  = 16
+  special = false
+  upper   = false
+  count   = 2
+}
+
+resource "azurerm_storage_account" "mystoragebox" {
+  count                    = 2
+  name                     = "mybox${random_string.storage_account_name_unique[count.index].result}"
+  resource_group_name      = azurerm_resource_group.myRG.name
+  location                 = azurerm_resource_group.myRG.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+
+
+
+}
+
