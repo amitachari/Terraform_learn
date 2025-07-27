00df5da88dcc436355dcf9208d06975da4b688ee
Amit Achari <amit.achari06@outlook.com>
2025-07-27 20:35:58 +0530

    added splat code

---
+++ terraform_splat/provider.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_version = ">=1.10"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.0.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.7.2"
+    }
+
+  }
+}
+
+provider "azurerm" {
+  features {}
+
+}
